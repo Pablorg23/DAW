ee7a4c8e83c35f707ce5686c93301017e92605a4
Pablorg23 <pabloramirez.smr@gmail.com>
2023-10-04 12:03:21 +0200

    actu

---
+++ Desarrollo_Web_en_Cliente/Tema1/EjerciciosPropuestos2.2/script.js
@@ -0,0 +1,10 @@
+var persona={
+    nombre:"Pepe",
+    edad: 58,
+    altura:200
+}
+
+function mostrar(){
+window.alert("El nombre es de "+persona.nombre+" con una edad "+persona.edad+" con una altura de "+persona.altura);
+
+}
