7892e8341c7308c5c2568b43797461e57663813e
Pablorg23 <pabloramirez.smr@gmail.com>
2023-10-10 12:10:52 +0200

    69

--- Desarrollo_Web_en_Cliente/Tema2/EjercicioRepaso/script.js
+++ Desarrollo_Web_en_Cliente/Tema2/EjercicioRepaso/script.js
@@ -17,6 +17,41 @@ function mostrarInformacion(){
     const edadEnDias=edad*365;
     window.alert("Tu nombre es "+Nombre+" y tu edad en dias es de "+edadEnDias);
 
+}
+function calcular() {
 
+    const numeroIngresado = parseFloat(document.getElementById("numero").value);
 
+    if (!isNaN(numeroIngresado)) {
+        const doble = numeroIngresado * 2;
+        const triple = numeroIngresado * 3;
+        const cuadruple = numeroIngresado * 4;
+
+        const resultado = `El doble es: ${doble}, el triple es: ${triple}, el cuádruple es: ${cuadruple}`;
+        
+        alert(resultado);
+    } else {
+        alert("Por favor, ingresa un número válido.");
+    }
+}
+function calcular2() {
+    const radio = parseFloat(document.getElementById("radio").value);
+
+    if (!isNaN(radio)) {
+        function calcularLongitudCircunferencia(radio) {
+            return 2 * Math.PI * radio;
+        }
+
+        function calcularAreaCirculo(radio) {
+            return Math.PI * Math.pow(radio, 2);
+        }
+
+        const longitudCircunferencia = calcularLongitudCircunferencia(radio);
+        const areaCirculo = calcularAreaCirculo(radio);
+
+        document.getElementById("longitud").textContent = longitudCircunferencia.toFixed(2);
+        document.getElementById("area").textContent = areaCirculo.toFixed(2);
+    } else {
+        alert("Por favor, ingresa un radio válido.");
+    }
 }
