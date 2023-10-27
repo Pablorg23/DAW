44b657b739514b70222fb0624e8019584587925b
Pablorg23 <pabloramirez.smr@gmail.com>
2023-10-27 11:14:08 +0200

    actu

--- Programacion/Tema3/U3_AA_Ej14_Pablo.java
+++ Programacion/Tema3/U3_AA_Ej14_Pablo.java
@@ -1,45 +1,35 @@
    import java.util.Scanner;
 
 public class U3_AA_Ej14_Pablo {
-
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.print("Introduce un número n: ");
+        System.out.print("Introduce un número entero positivo n: ");
         int n = teclado.nextInt();
-        teclado.close();
 
-        int countPrimos = contarNumerosPrimos(n);
+        int countPrimos = 0; // Inicializamos el contador de números primos
+
+        for (int i = 2; i <= n; i++) {
+            if (esPrimo(i)) {
+                countPrimos++;
+            }
+        }
+
         System.out.println("Hay " + countPrimos + " números primos entre 1 y " + n);
+
+        teclado.close();
     }
 
-    public static boolean esPrimo(int num) {
-        if (num <= 1) {
+    // Método para comprobar si un número es primo
+    public static boolean esPrimo(int numero) {
+        if (numero <= 1) {
             return false;
         }
-        if (num <= 3) {
-            return true;
-        }
-        if (num % 2 == 0 || num % 3 == 0) {
-            return false;
-        }
-
-        for (int i = 5; i * i <= num; i += 6) {
-            if (num % i == 0 || num % (i + 2) == 0) {
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
                 return false;
             }
         }
-
         return true;
-    }
-
-    public static int contarNumerosPrimos(int n) {
-        int count = 0;
-        for (int i = 1; i <= n; i++) {
-            if (esPrimo(i)) {
-                count++;
-            }
-        }
-        return count;
     }
 }
