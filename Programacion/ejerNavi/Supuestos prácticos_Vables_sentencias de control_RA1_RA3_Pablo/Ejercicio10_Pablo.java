25c607a9b52a4c52669b2aa8556385d39312a349
Pablorg23 <pabloramirez.smr@gmail.com>
2024-01-09 15:23:19 +0100

    actu

--- Programacion/ejerNavi/Supuestos prácticos_Vables_sentencias de control_RA1_RA3_Pablo/Ejercicio10_Pablo.java
+++ Programacion/ejerNavi/Supuestos prácticos_Vables_sentencias de control_RA1_RA3_Pablo/Ejercicio10_Pablo.java
@@ -3,62 +3,43 @@ public class Ejercicio10_Pablo {
     public static void main(String[] args) {
         //a
         int edad;
-
         //b
         String codigoPostal;
-
         //c
         double altura;
-
         //d
         char genero;
-
         //e
         String nombre;
-
         //f
         int numeroDeHijos;
-
         //g
         double iva;
-
         //h
         int tallaCamisa;
-
         //i
         double peso;
-
         //j
         double precio;
-
         //k
         boolean alumnoRepetidor;
-
         //l
         String mensaje;
-
         //m
         char letra;
-
         //n
         boolean mayorEdad;
-
         //o
         int minutos;
-
         //p
         int dias;
-
         //q
         String matriculaCoche;
-
         //r
         int contador;
-
         //s
         boolean mayorDeEdad;
-
         //t
-        String tallaCamiseta;
+        String tallaCamiseta;// no uso char ya que si es una XL ya son 2 caracteres
     }
 }
